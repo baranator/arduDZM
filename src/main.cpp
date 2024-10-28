4e2ea31d0ca1f0c634ae0b0ce33ebbe3b31c9b10
Eike Baran <eikebaran89@gmail.com>
2024-10-28 17:08:35 +0100

    work work work; display parts and first ideas of rpm-measurement

--- src/main.cpp
+++ src/main.cpp
@@ -1,35 +1,108 @@
 #include <Arduino.h>
-
 #include <Servo.h>
-
 #include <TinyWireM.h>
 #include <Tiny4kOLED.h>
-
-uint8_t oledWidth = 128;
-uint8_t oledHeight = 64;
-
 #include "m38_font.h"
-const DCfont *oledFont = FONTM38;
-
-Servo myservo;  // create Servo object to control a servo
 
 
+#define DEBUG
+#define RPM_MAX 8000
+#define RPM_CRIT 6000
+#define WELCOME_MSG "Hidiho!"
+
+
+
+
+
+
+//const DCfont *oledFont = FONTM38;
+
+//Servo myservo;  // create Servo object to control a servo
+
+const uint8_t oledWidth = 128;
+const uint8_t oledHeight = 64;
 #define ADC_PIN 5
 uint16_t adcMax=0;
 uint16_t adcMin=1023;
+//low voltage count to detect spark safely
+const uint8_t lvths=5;
+uint8_t lvcount=0;
+
+unsigned long lastSpark=0;
 
 
 void oledSetup(){
-
+  TinyWireM.begin();
+  oled.begin(oledWidth, oledHeight, sizeof(tiny4koled_init_128x64br), tiny4koled_init_128x64br);
+  oled.clear();
+  oled.setFont(FONT6X8);
+  oled.on();
+  oled.setCursor(30, 4); // x: pixel / y: line
+  oled.print("Hello world");
 
 }
 
-void oledShowRPM(){
+void drawBar(uint16_t rpm){
+  uint8_t e=0b00000000;
+  uint8_t f=0b11111111;
+  uint16_t segs=(RPM_MAX/1000.0);
+  uint16_t segw=128/segs;
 
+  uint16_t segf=round(rpm/1000.0);
+
+
+    if(segw%2!=0){
+      segw--;
+    }
+    oled.setCursor(0, 0);
+    oled.startData();
+    for (uint8_t x=0; x<segs; x++) {
+      //invert colors for reached ticks
+      if(segf>x){
+        e=0b00000000;
+        f=0b11111111;
+      }else{
+        f=0b00000000;
+        e=0b11111111;
+      }
+      //mark critical rpms
+      if(x>RPM_CRIT/1000){
+          f=f^0b00011000;
+          e=e^0b00011000;
+      }
+      for(int i=0;i<segw-1;i++){
+        oled.sendData(f);
+      }
+      //tick
+      oled.sendData(e);
+    }
+    oled.endData();
+
+}
+
+
+void oledShowRPM(uint16_t rpm){
+  //BAR
+  drawBar(rpm);
+  //TEXT
+  oled.setFontX2(FONTM38);
+  if(rpm>RPM_CRIT){
+    oled.setCursor(5, 2);
+    oled.print("(!)");
+  }
+  oled.setCursor(32, 2);
+  oled.print(rpm);
+  
 }
 
 void oledDebugLine(){
-
+  oled.setCursor(1, oledHeight/8-1); // x: pixel / y: line
+  oled.setFont(FONT6X8);
+  String d="min: ";
+  d+=adcMin;
+  d+=", max: ";
+  d+=adcMax;
+  oled.print(d.c_str());
 }
 
 void updateMinMax(){
@@ -42,24 +115,60 @@ void updateMinMax(){
   }
 }
 
+void oledShow(){
+  oled.clear();
+  #ifdef DEBUG
+    oledDebugLine();
+  #endif
+
+
+}
+
 void setup() {
   //Serial.begin(9600);
   // initialize digital pin LED_BUILTIN as an output.
   //pinMode(PB3, OUTPUT);
   //myservo.attach(PB4);
-
-
-   TinyWireM.begin();
-  oled.begin(oledWidth, oledHeight, sizeof(tiny4koled_init_128x64br), tiny4koled_init_128x64br);
-  oled.clear();
-  oled.setFont(FONT6X8);
-  oled.on();
+  oledSetup();
   oled.setCursor(30, 4); // x: pixel / y: line
-  oled.print("Hello world");
+  oled.print(WELCOME_MSG);
+
+  lastSpark=micros();
 }
 
 void loop() {
-  updateMax();
+  updateMinMax();
+  
+  if(adcMax>adcMin){
+    uint8_t val=analogRead(ADC_PIN);
+    if(val < adcMax-0.2*(adcMax-adcMin)){
+      lvcount++;
+    }else{
+      lvcount--;
+    }
+    if(lvcount<0){
+      lvcount=0;
+    }
+    if(lvcount>=lvths){
+      unsigned long currentSpark=micros();
+      unsigned long periodLength;
+      //handle overflow after ~70min
+      if(lastSpark>currentSpark){
+        periodLength=currentSpark;
+      }else{
+        periodLength=currentSpark-lastSpark;
+      }
+      lastSpark=currentSpark;
+
+      //store in period array, check if 5 periods summed up, if so, calc arith. mean, then reset array/conzter
+
+
+      lvcount=0;
+    }
+  }
+
+
+
   // // put your main code here, to run repeatedly:
   //   digitalWrite(PB3, HIGH);   // turn the LED on (HIGH is the voltage level)
   // myservo.write(0);
@@ -67,9 +176,4 @@ void loop() {
   // Serial.println("switchi");
   // digitalWrite(PB3, LOW);    // turn the LED off by making the voltage LOW
   // myservo.write(180);
-    oled.setCursor(30, 4); // x: pixel / y: line
-  oled.print("Hello world");
-  delay(2000);                       // wait for a second
-    oled.setCursor(30, 4); // x: pixel / y: line
-  oled.print("bye world");
 }
